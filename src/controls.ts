4e6b43dad5297f7e3e14d1ccbe24103d3f0474e9
Jeff Fohl <jeff@fohl.com>
2023-09-20 14:41:55 -0700

    wip

---
+++ src/controls.ts
@@ -0,0 +1,35 @@
+const aboutMeButton = document.getElementById('about-me')
+const whatIsThisButton = document.getElementById('what-is-this')
+const bioPanel = document.getElementById('bio')
+const aboutPanel = document.getElementById('about')
+
+let bioPanelOpen = false
+let aboutPanelOpen = false
+
+whatIsThisButton?.addEventListener('click', (_e: MouseEvent) => {
+    if (aboutPanelOpen && aboutPanel) {
+        aboutPanel.style.width = '0px'
+        aboutPanelOpen = false
+    } else if (!aboutPanelOpen && aboutPanel) {
+        if (bioPanelOpen && bioPanel) {
+            bioPanel.style.width = '0px'
+            bioPanelOpen = false
+        }
+        aboutPanel.style.width = '800px'
+        aboutPanelOpen = true
+    }
+})
+
+aboutMeButton?.addEventListener('click', (_e: MouseEvent) => {
+    if (bioPanelOpen && bioPanel) {
+        bioPanel.style.width = '0px'
+        bioPanelOpen = false
+    } else if (!bioPanelOpen && bioPanel) {
+        if (aboutPanelOpen && aboutPanel) {
+            aboutPanel.style.width = '0px'
+            aboutPanelOpen = false
+        }
+        bioPanel.style.width = '800px'
+        bioPanelOpen = true
+    }
+})
