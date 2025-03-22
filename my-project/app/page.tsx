bc1dfa44a0a2a42ebc853573b10aa853ed216f3a
Jeff Fohl <jeff@fohl.com>
2025-03-22 07:50:49 -0700

    better button

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -22,7 +22,7 @@ export default function GridCanvas() {
         <div className="overflow-hidden absolute top-[60px] left-0 w-full h-[calc(100%-60px)] lg:w-[calc(100%-200px)] lg:h-full lg:top-0 lg:left-[200px]">
             <div
                 onClick={toggleAboutPanel}
-                className="absolute top-4 right-4 w-[35px] h-[35px] rounded-full bg-[#22222244] hover:bg-[#444444] z-20 flex items-center justify-center text-white text-xl cursor-pointer"
+                className="transition absolute top-4 right-4 w-[35px] h-[35px] rounded-full bg-[#22222244] hover:bg-[#22222299] z-20 flex items-center justify-center text-white text-xl cursor-pointer"
             >
                 {aboutHidden ? '?' : 'X'}
             </div>
