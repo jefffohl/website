ab38c960d9f28eabcc2f9496dd9617d4ad6382f8
Jeff Fohl <jeff@fohl.com>
2025-03-22 07:36:31 -0700

    handle position of about panel, and delete unused components

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -19,10 +19,10 @@ export default function GridCanvas() {
     }, [])
 
     return (
-        <div className="absolute top-[60px] left-0 w-full h-[calc(100%-60px)] lg:w-[calc(100%-200px)] lg:h-full lg:top-0 lg:left-[200px]">
+        <div className="overflow-hidden absolute top-[60px] left-0 w-full h-[calc(100%-60px)] lg:w-[calc(100%-200px)] lg:h-full lg:top-0 lg:left-[200px]">
             <div
                 onClick={toggleAboutPanel}
-                className="absolute top-4 right-4 w-[35px] h-[35px] rounded-full bg-[#22222244] hover:bg-[#444444] z-10 flex items-center justify-center text-white text-xl cursor-pointer"
+                className="absolute top-4 right-4 w-[35px] h-[35px] rounded-full bg-[#22222244] hover:bg-[#444444] z-20 flex items-center justify-center text-white text-xl cursor-pointer"
             >
                 {aboutHidden ? '?' : 'X'}
             </div>
