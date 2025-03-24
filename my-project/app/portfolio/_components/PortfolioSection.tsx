ee802d43bcdd889cf21a4e1c4ab65da4cf417fa7
Jeff Fohl <jeff@fohl.com>
2025-03-23 18:37:51 -0700

    portfolio content

--- my-project/app/portfolio/_components/PortfolioSection.tsx
+++ my-project/app/portfolio/_components/PortfolioSection.tsx
@@ -7,9 +7,11 @@ export interface PortfolioSectionProps {
     title: string
     description: React.ReactNode
     company: string
+    companyUrl: string
     assets: PortfolioAsset[]
     date: string
     roles: string[]
+    stack?: string[]
 }
 
 export interface PortfolioAsset {
@@ -17,8 +19,8 @@ export interface PortfolioAsset {
     thumbnail: ImageProps
     src: string
     alt: string
-    width: number
-    height: number
+    width: number | string
+    height: number | string
 }
 
 export enum PortfolioAssetType {
@@ -30,30 +32,40 @@ export default function PortfolioSection({
     title,
     description,
     company,
+    companyUrl,
     assets,
     date,
     roles,
+    stack,
 }: PortfolioSectionProps) {
     const [activeAssetIndex, setActiveAssetIndex] = useState(0)
     return (
-        <section>
+        <section className="p-[4rem_0] border-t border-b border-t-[#555] border-b-[#222]">
             <div className="flex lg:flex-row flex-col">
-                <div className="lg:flex-[0_0_30rem] flex-1 lg:pr-8 pr-0">
+                <div className="lg:flex-[0_0_auto] w-[31rem] flex-1 lg:pr-8 pr-0">
                     <h2>{title}</h2>
                     {description}
                     <dl>
                         <dt>Date</dt>
                         <dd>{date}</dd>
                         <dt>Company</dt>
-                        <dd>{company}</dd>
+                        <dd>
+                            <a href={companyUrl}>{company}</a>
+                        </dd>
                         <dt>Roles</dt>
                         <dd>{roles.join(', ')}</dd>
+                        {stack && (
+                            <>
+                                <dt>Stack</dt>
+                                <dd>{stack.join(', ')}</dd>
+                            </>
+                        )}
                     </dl>
-                    <div className="flex mb-4">
+                    <div className="flex flex-wrap">
                         {assets.map((asset, i) => (
                             <div
                                 key={'asset_' + i}
-                                className={`asset-thumbnail transition-all duration-300 lg:w-[150px] w-[100px] shadow-md rounded overflow-hidden mr-4 cursor-pointer ${
+                                className={`asset-thumbnail transition-all duration-300 shadow-md rounded overflow-hidden mr-4 mb-4 cursor-pointer ${
                                     i === activeAssetIndex
                                         ? 'ring-2 ring-[var(--link-color)]'
                                         : ''
@@ -65,6 +77,7 @@ export default function PortfolioSection({
                                     alt={asset.thumbnail.alt}
                                     width={asset.thumbnail.width}
                                     height={asset.thumbnail.height}
+                                    quality={100}
                                 />
                             </div>
                         ))}
@@ -76,16 +89,20 @@ export default function PortfolioSection({
                         <Image
                             src={assets[activeAssetIndex].src}
                             alt={assets[activeAssetIndex].alt}
-                            width={assets[activeAssetIndex].width}
-                            height={assets[activeAssetIndex].height}
+                            width={Number(assets[activeAssetIndex].width)}
+                            height={Number(assets[activeAssetIndex].height)}
+                            quality={100}
                         />
                     ) : (
-                        <video
-                            src={assets[activeAssetIndex].src}
-                            controls
+                        <iframe
                             width={assets[activeAssetIndex].width}
                             height={assets[activeAssetIndex].height}
-                        />
+                            src={assets[activeAssetIndex].src}
+                            title={assets[activeAssetIndex].alt}
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerPolicy="strict-origin-when-cross-origin"
+                            allowFullScreen
+                        ></iframe>
                     )}
                 </div>
             </div>
