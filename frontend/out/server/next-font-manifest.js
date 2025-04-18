09dc168f0330237cc8fd071a62e5dfe5148e1713
Jeff Fohl <jeff@fohl.com>
2025-04-18 11:50:37 -0700

    delete build artifacts and update .gitignore

--- frontend/out/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/jefffohl/dev/website/frontend/app/layout\":[\"static/media/3d9ea938b6afa941-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
