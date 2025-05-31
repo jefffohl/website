63cad8ac09a7c35eb238e1ee93959d482f54f46d
Jeff Fohl <jeff@fohl.com>
2025-05-31 08:07:50 -0700

    remove erroneous tailwind classes

--- frontend/app/blog/_components/Blocks.tsx
+++ frontend/app/blog/_components/Blocks.tsx
@@ -17,25 +17,25 @@ export const blocks: BlocksComponents = {
         )
     },
     paragraph: ({ children }) => (
-        <p className="text-neutral900 max-w-prose xl:text-lg">{children}</p>
+        <p className="max-w-prose xl:text-lg">{children}</p>
     ),
     // ...or point to a design system
     heading: ({ children, level }) => {
         switch (level) {
             case 1:
-                return <h1 className="text-neutral900 text-4xl">{children}</h1>
+                return <h1 className="text-4xl">{children}</h1>
             case 2:
-                return <h2 className="text-neutral900 text-2xl">{children}</h2>
+                return <h2 className="text-2xl">{children}</h2>
             case 3:
-                return <h3 className="text-neutral900 text-xl">{children}</h3>
+                return <h3 className="text-xl">{children}</h3>
             case 4:
-                return <h4 className="text-neutral900 text-lg">{children}</h4>
+                return <h4 className="text-lg">{children}</h4>
             case 5:
-                return <h5 className="text-neutral900 text-base">{children}</h5>
+                return <h5 className="text-base">{children}</h5>
             case 6:
-                return <h6 className="text-neutral900 text-sm">{children}</h6>
+                return <h6 className="text-sm">{children}</h6>
             default:
-                return <h1 className="text-neutral900 text-4xl">{children}</h1>
+                return <h1 className="text-4xl">{children}</h1>
         }
     },
     // For links, you may want to use the component from your router or framework
