ea0ce385b4403820bbeedafcb06a02a9713169f4
Jeff Fohl <jeff@fohl.com>
2026-02-27 18:08:49 -0800

    bloom-2

--- frontend/animations/bloom2.ts
+++ frontend/animations/bloom2.ts
@@ -4,7 +4,7 @@
  *  Copyright © 2026 Jeff Fohl
  */
 
-import { gaussianRandom } from '@/animations/utils/random'
+import { gaussianRandom, rampedRandom } from '@/animations/utils/random'
 
 // types
 type Point = { x: number; y: number; variance: number }
@@ -30,44 +30,11 @@ class Bloom2Animator {
         }
     }
 
-    /**
-     * Deform a polygon
-     */
-    private deformPolygon(polygon: Polygon, radius: number): Polygon {
-        const newPoly: Polygon = []
-        for (let i = 0; i < polygon.length; i++) {
-            const pointA = polygon[i]
-            if (!pointA) {
-                continue
-            }
-            const A = { x: pointA.x, y: pointA.y, variance: pointA.variance }
-            const n: number = (i + 1) % polygon.length
-            const pointC = polygon[n]
-            if (!pointC) {
-                continue
-            }
-            const C = { x: pointC.x, y: pointC.y, variance: pointC.variance }
-            if (!A || !C) {
-                continue
-            }
-            const variance = gaussianRandom(
-                (A.variance + C.variance) / 2,
-                radius / 20
-            )
-            const B = {
-                x: gaussianRandom((A.x + C.x) / 2, variance),
-                y: gaussianRandom((A.y + C.y) / 2, variance),
-                variance,
-            }
-            newPoly.push(A, B, C)
-        }
-        return newPoly
-    }
-
     private createPolygon(
         coordinates: { x: number; y: number },
         radius: number,
-        facets: number
+        facets: number,
+        variance: number
     ): Polygon {
         if (!coordinates || !radius || !facets) {
             return []
@@ -89,27 +56,70 @@ class Bloom2Animator {
                 continue
             }
             let x = Math.round(coordinates.x + radius * Math.cos(angle))
-
             if (x < 0) x = 0 // if the coordinate goes off the edge, just keep it on the edge
             let y = Math.round(coordinates.y + radius * Math.sin(angle))
-
-            let variance = Math.random() * (radius / 3)
-
             if (y < 0) y = 0
             points.push({ x, y, variance })
         }
 
         return points
     }
+    /**
+     * Deform a polygon, by moving the points in a random direction
+     */
+    private deformPolygon(polygon: Polygon): Polygon {
+        const newPoly: Polygon = []
+        for (let i = 0; i < polygon.length; i++) {
+            newPoly.push({
+                x: gaussianRandom(polygon[i].x, polygon[i].variance),
+                y: gaussianRandom(polygon[i].y, polygon[i].variance),
+                variance: polygon[i].variance,
+            })
+        }
+        return newPoly
+    }
+
+    /**
+     * Deform a polygon by imputing points between the existing points and the new points
+     */
+    private deformAndImputePolygon(polygon: Polygon): Polygon {
+        const newPoly: Polygon = []
+        for (let i = 0; i < polygon.length; i++) {
+            const pointA = polygon[i]
+            if (!pointA) {
+                continue
+            }
+            const A = { x: pointA.x, y: pointA.y, variance: pointA.variance }
+            const n: number = (i + 1) % polygon.length
+            const pointC = polygon[n]
+            if (!pointC) {
+                continue
+            }
+            const C = { x: pointC.x, y: pointC.y, variance: pointC.variance }
+            if (!A || !C) {
+                continue
+            }
+            const v = gaussianRandom(
+                (pointA.variance + pointC.variance) / 2,
+                (pointA.variance + pointC.variance) / 2
+            )
+            const B = {
+                x: gaussianRandom((A.x + C.x) / 2, v),
+                y: gaussianRandom((A.y + C.y) / 2, v),
+                variance: v,
+            }
+            newPoly.push(A, B, C)
+        }
+        return newPoly
+    }
 
     private iterativelyDeformPolygon(
         polygon: Polygon,
-        radius: number,
         iterations: number
     ): Polygon {
         let deformedPolygon = polygon
         for (let i = 0; i < iterations; i++) {
-            deformedPolygon = this.deformPolygon(deformedPolygon, radius)
+            deformedPolygon = this.deformAndImputePolygon(deformedPolygon)
         }
         return deformedPolygon
     }
@@ -150,8 +160,8 @@ class Bloom2Animator {
         // create a random number of polygons:
         // get the larger of the dimensions
         const larger = Math.max(this.rect!.width, this.rect!.height)
-        let radius = Math.round((Math.random() * larger) / 4.72)
-        let facets = Math.round(Math.random() * 10)
+        let radius = Math.round((Math.random() * larger) / 6)
+        let facets = Math.round(gaussianRandom(15, 5))
         if (facets < 5) {
             facets = 5
         }
@@ -159,13 +169,13 @@ class Bloom2Animator {
         const x = Math.round(Math.random() * (this.rect!.width || 100))
         const y = Math.round(Math.random() * (this.rect!.height || 100))
         // calculate random color
-        const r = Math.round(Math.random() * 256)
-        const g = Math.round(Math.random() * 256)
-        const b = Math.round(Math.random() * 256)
-        const color = `rgba(${r},${g},${b},0.025)`
+        const l = Math.random() * 100
+        const c = Math.random() * 100
+        const h = Math.random() * 360
+        const color = `oklch(${l}% ${c}% ${h}deg / 0.025)`
 
-        const poly = this.createPolygon({ x, y }, radius, facets)
-        const deformedPoly = this.deformPolygon(poly, radius)
+        const poly = this.createPolygon({ x, y }, radius, facets, radius / 10)
+        const deformedPoly = this.deformPolygon(poly)
         const numberOfLayers = 85
         let iteration = 0
         let self = this
@@ -173,7 +183,7 @@ class Bloom2Animator {
         function draw() {
             iteration++
             self.drawPolygon(
-                self.iterativelyDeformPolygon(deformedPoly, radius, 5),
+                self.iterativelyDeformPolygon(deformedPoly, 6),
                 color
             )
             self.timer = window.setTimeout(() => {
